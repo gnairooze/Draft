ee92f645e4fc76c53187924a389584444908038a
George Nairooze <george.nairooze@linkdev.com>
2020-12-10 21:40:16 +0200

    add demo auth

---
+++ Solutions/DemoAuth/DemoAuth.WWW/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoAuth.WWW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.WindowsIdentityName = WindowsIdentity.GetCurrent().Name;
+            ViewBag.UserName = User.Identity.Name;
+            ViewBag.UserIsAuthenticated = User.Identity.IsAuthenticated;
+            ViewBag.UserAuthType = User.Identity.AuthenticationType;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
