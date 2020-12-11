04549dd353632cf71fda693d50cefcdb2cebf013
George Nairooze <gnairooze@gmail.com>
2020-12-11 15:10:09 +0200

    modify remove

--- PS-Scripts/change-ip-address.ps1
+++ PS-Scripts/change-ip-address.ps1
@@ -1,3 +1,4 @@
+# if gateway changed, you will end with 2 gateways per ip address and generate errors later on. This script needs more work
 $gateway = "192.168.1.1"
 $interface = "Ethernet"
 $ip = "192.168.1.149"
@@ -7,7 +8,7 @@ Write-Host "Before"
 Get-NetIPConfiguration -InterfaceAlias $interface
 
 Write-Host "Update started"
-Remove-NetIPAddress –InterfaceAlias $interface -IPAddress $ip -DefaultGateway $gateway
+Remove-NetIPAddress –InterfaceAlias $interface -IPAddress $ip
 New-NetIPAddress –InterfaceAlias $interface –AddressFamily IPv4 -IPAddress $ip –PrefixLength 24 -DefaultGateway $gateway
 Set-DnsClientServerAddress -InterfaceAlias $interface -ServerAddresses $dns
 Write-Host "Update completed"
