5bdb93337e36d128aace4a0236a88d2c9efb3acd
George Nairooze <gnairooze@gmail.com>
2020-12-26 11:41:10 +0200

    enhance script

--- PS-Scripts/restart-device.ps1
+++ PS-Scripts/restart-device.ps1
@@ -1,5 +1,11 @@
 $deviceName = 'Qualcomm Atheros AR3012 Bluetooth 4.0'
 
-Get-PnpDevice -PresentOnly -FriendlyName $deviceName | Disable-PnpDevice
-Get-PnpDevice -FriendlyName $deviceName | Enable-PnpDevice
+$device = Get-PnpDevice -PresentOnly -FriendlyName $deviceName
+Write-Host "disable $deviceName"
+Disable-PnpDevice -InstanceId $device.InstanceId -Confirm:$false
+Write-Host "sleep for 10 seconds"
+Start-Sleep -Seconds 10
+Write-Host "enable $deviceName"
+Enable-PnpDevice -InstanceId $device.InstanceId -Confirm:$false
+
 Get-PnpDevice -PresentOnly -FriendlyName $deviceName | Select-Object Status, PNPClass, Caption, DeviceID
