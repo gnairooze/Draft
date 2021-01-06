a67270bd3083beb65bba4d5b4c2f747ac4b90dfa
George Nairooze <gnairooze@gmail.com>
2021-01-06 13:11:52 +0200

    ps script to find text in files

---
+++ PS-Scripts/search-text-in-files.ps1
@@ -0,0 +1,19 @@
+﻿
+$Path = "C:\temp\1\IP-Notify"
+$Text = "Serilog"
+$PathArray = [System.Collections.ArrayList]@()
+$Results = "C:\temp\test.txt"
+
+$Files = Get-ChildItem $Path -Filter "*.*" | Where-Object { $_.Attributes -ne "Directory"}
+
+Get-ChildItem $Path -Filter "*.*" | Where-Object { $_.Attributes -ne "Directory"} |
+    ForEach-Object {
+        If (Get-Content $_.FullName | Select-String -Pattern $Text) {
+            $PathArray += $_.FullName
+    }
+}
+
+
+Write-Host $PathArray
+Set-Content -Path $Results $PathArray
+
