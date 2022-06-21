532eee66f0858b74f67a05d79a95abf6a58b65f2
George Nairooze <george.nairooze@linkdev.com>
2022-06-21 12:28:05 +0200

    get http request sample

---
+++ Solutions/APIClient/CallAPI/Caller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallAPI
+{
+    internal class Caller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+    }
+}
